b98a03cd09a408b2f139c4fc51bc8be6f21ce91b
Jaehwan Jeong <31501015+sepiros62@users.noreply.github.com>
2020-09-10 12:01:06 +0900

    Update mian.tf

--- mian.tf
+++ mian.tf
@@ -8,7 +8,7 @@ module "vpc" {
   cidr = "10.50.0.0/16"
 
   azs = ["ap-northeast-2a", "ap-northeast-2c"]
-  private_subnets = ["10.50.100.0/24", "10.50.101.0/24", "10.50.102.0/24"]
+  private_subnets = ["10.50.100.0/24", "10.50.101.0/24"]
 
   enable_nat_gateway = "true"
   one_nat_gateway_per_az = "false"
