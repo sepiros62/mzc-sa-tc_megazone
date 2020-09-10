9865c55ca4b5b8ae66f3884d137258020a23cc98
jeong.jaehwan <sepiros62@gmail.com>
2020-09-10 02:54:19 +0000

    Update | Terrafom VPC Module Main.tf - Terrafom VPC Module을 사용 - Main.tf 파일 업로드

--- mian.tf
+++ mian.tf
@@ -1,22 +1,4 @@
 //--------------------------------------------------------------------
-// Variables
-variable "vpc_database_subnet_assign_ipv6_address_on_creation" {}
-variable "vpc_default_security_group_egress" {}
-variable "vpc_default_security_group_ingress" {}
-variable "vpc_ec2messages_endpoint_subnet_ids" {}
-variable "vpc_elasticache_subnet_assign_ipv6_address_on_creation" {}
-variable "vpc_enable_classiclink" {}
-variable "vpc_enable_classiclink_dns_support" {}
-variable "vpc_flow_log_cloudwatch_log_group_kms_key_id" {}
-variable "vpc_flow_log_cloudwatch_log_group_retention_in_days" {}
-variable "vpc_flow_log_log_format" {}
-variable "vpc_intra_subnet_assign_ipv6_address_on_creation" {}
-variable "vpc_private_subnet_assign_ipv6_address_on_creation" {}
-variable "vpc_public_subnet_assign_ipv6_address_on_creation" {}
-variable "vpc_redshift_subnet_assign_ipv6_address_on_creation" {}
-variable "vpc_vpn_gateway_az" {}
-
-//--------------------------------------------------------------------
 // Modules
 module "vpc" {
   source  = "app.terraform.io/megazonesa/vpc/aws"
