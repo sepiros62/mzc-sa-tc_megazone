4889839da36fd294bd7b7657873fc6381b85bd28
jaehwan <sepiros62@gmail.com>
2020-09-22 18:15:26 +0900

    Add | input variable

--- main.tf
+++ main.tf
@@ -2,97 +2,95 @@
 // Modules
 //--------------------------------------------------------------------
 
-######
-# VPC
-######
-module "vpc" {
-  source = "app.terraform.io/megazonesa/vpc/aws"
-  version = "2.48.0"
-  
-  name		= var.name
-  cidr		= var.cidr
-  azs 		= var.azs
-
-  database_subnets    =  var.database_subnets
-
-  create_database_subnet_group           = true
-  create_database_subnet_route_table     = true
+###############
+# Data sources 
+###############
+data "aws_vpc" "default" {
+  default = true
 }
 
+data "aws_subnet_ids" "all" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+data "aws_security_group" "default" {
+  vpc_id = data.aws_vpc.default.id
+  name   = "default"
+}
 
 #####
 # DB
 #####
-#module "db" {
-#  source = "app.terraform.io/megazonesa/rds/aws"
-#  version = "~> 2.0"
-#
-#  identifier = var.identifier
-#
-#  # All available versions: http://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/CHAP_MySQL.html#MySQL.Concepts.VersionMgmt
-#  engine            = "mysql"
-#  engine_version    = var.engin_version
-#  instance_class    = var.instance_class
-#  allocated_storage = var.allocated_storage
-#  storage_encrypted = false
-#
-#  name     = var.name
-#  username = var.username
-#  password = var.password
-#  port     = "3306"
-#
-#  vpc_security_group_ids = [data.aws_security_group.default.id]
-#
-#  multi_az = var.maulti_az
-#
-#  # disable backups to create DB faster
-#  backup_retention_period = "7"
-#  maintenance_window = "Mon:00:00-Mon:03:00"
-#  backup_window      = "03:00-06:00"
-#
-#  tags = var.tags
-#
-#  # DB subnet group
-#  #subnet_ids =  data.aws_subnet_ids.all.ids
-#  subnet_ids =  module.vpc.aws_subnet_ids.all.ids
-#
-#  # DB parameter group
-#  family = var.family
-#
-#  # DB option group
-#  major_engine_version = var.major_engine_version
-#
-#  # Snapshot name upon DB deletion
-#  final_snapshot_identifier = var.final_snapshot_identifier
-#
-#  # Database Deletion Protection
-#  deletion_protection = var.deletion_protection
-#
-#  parameters = [
-#    {
-#      name  = "character_set_client"
-#      value = "utf8"
-#    },
-#    {
-#      name  = "character_set_server"
-#      value = "utf8"
-#    }
-#  ]
-#
-#  options = [
-#    {
-#      option_name = "MARIADB_AUDIT_PLUGIN"
-#
-#      option_settings = [
-#        {
-#          name  = "SERVER_AUDIT_EVENTS"
-#          value = "CONNECT"
-#        },
-#        {
-#          name  = "SERVER_AUDIT_FILE_ROTATIONS"
-#          value = "37"
-#        },
-#      ]
-#    },
-#  ]
-#}
+module "db" {
+  source = "app.terraform.io/megazonesa/rds/aws"
+  version = "~> 2.0"
+
+  identifier = var.identifier
+
+  # All available versions: http://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/CHAP_MySQL.html#MySQL.Concepts.VersionMgmt
+  engine            = "mysql"
+  engine_version    = var.engin_version
+  instance_class    = var.instance_class
+  allocated_storage = var.allocated_storage
+  storage_encrypted = false
+
+  name     = var.name
+  username = var.username
+  password = var.password
+  port     = "3306"
+
+  vpc_security_group_ids = [data.aws_security_group.default.id]
+
+  multi_az = var.maulti_az
+
+  # disable backups to create DB faster
+  backup_retention_period = "7"
+  maintenance_window = "Mon:00:00-Mon:03:00"
+  backup_window      = "03:00-06:00"
+
+  tags = var.tags
+
+  # DB subnet group
+  #subnet_ids =  data.aws_subnet_ids.all.ids
+  subnet_ids =  module.vpc.aws_subnet_ids.all.ids
+
+  # DB parameter group
+  family = var.family
+
+  # DB option group
+  major_engine_version = var.major_engine_version
+
+  # Snapshot name upon DB deletion
+  final_snapshot_identifier = var.final_snapshot_identifier
+
+  # Database Deletion Protection
+  deletion_protection = var.deletion_protection
+
+  parameters = [
+    {
+      name  = "character_set_client"
+      value = "utf8"
+    },
+    {
+      name  = "character_set_server"
+      value = "utf8"
+    }
+  ]
+
+  options = [
+    {
+      option_name = "MARIADB_AUDIT_PLUGIN"
+
+      option_settings = [
+        {
+          name  = "SERVER_AUDIT_EVENTS"
+          value = "CONNECT"
+        },
+        {
+          name  = "SERVER_AUDIT_FILE_ROTATIONS"
+          value = "37"
+        },
+      ]
+    },
+  ]
+}
--- variables.tf
+++ variables.tf
@@ -1,43 +1,4 @@
 // -------
-// VPC
-// -------
-variable "create_vpc" {
-  description = "Controls if VPC should be created (it affects almost all resources)"
-  type        = bool
-  default     = false
-}
-
-variable "name" {
-  description = "Name to be used on all the resources as identifier"
-  type        = string
-  default     = ""
-}
-
-variable "cidr" {
-  description = "The CIDR block for the VPC. Default value is a valid CIDR, but not acceptable by AWS and should be overridden"
-  type        = string
-  default     = "0.0.0.0/0"
-}
-
-variable "azs" {
-  description = "A list of availability zones names or ids in the region"
-  type        = list(string)
-  default     = []
-}
-
-variable "database_subnets" {
-  description = "A list of database subnets"
-  type        = list(string)
-  default     = []
-}
-
-variable "create_database_subnet_route_table" {
-  description = "Controls if separate route table for database should be created"
-  type        = bool
-  default     = false
-}
-
-// -------
 // RDS
 // -------
 variable "identifier" {
