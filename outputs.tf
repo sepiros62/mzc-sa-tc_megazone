065f9ae56fe062169ff61597a91fb08cda328d46
jaehwan <sepiros62@gmail.com>
2020-09-22 21:52:06 +0900

    Add | input variable

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,35 @@
+# VPC
 output "vpc_id" {
   description = "The ID of the VPC"
-  value       = concat(aws_vpc.this.*.id, [""])[0]
+  value       = module.vpc.vpc_id
 }
 
+# CIDR blocks
 output "vpc_cidr_block" {
   description = "The CIDR block of the VPC"
-  value       = concat(aws_vpc.this.*.cidr_block, [""])[0]
+  value       = module.vpc.vpc_cidr_block
+}
+
+
+# Subnets
+output "private_subnets" {
+  description = "List of IDs of private subnets"
+  value       = module.vpc.private_subnets
+}
+
+output "public_subnets" {
+  description = "List of IDs of public subnets"
+  value       = module.vpc.public_subnets
+}
+
+# NAT gateways
+output "nat_public_ips" {
+  description = "List of public Elastic IPs created for AWS NAT Gateway"
+  value       = module.vpc.nat_public_ips
+}
+
+# AZs
+output "azs" {
+  description = "A list of availability zones spefified as argument to this module"
+  value       = module.vpc.azs
 }
